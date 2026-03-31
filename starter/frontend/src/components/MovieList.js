1fe9da1fb65af241a12253edfe5e6f75f80ed8c7
Nico <mickburog87@gmail.com>
2026-03-31 21:47:27 +0800

    Refactor axios call to improve readability

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
@@ -7,21 +6,15 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
-      .then((response) => {
-        setMovies(response.data.movies);
-      });
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+      setMovies(response.data.movies);
+    });
   }, []);
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li
-          className="movieItem"
-          key={movie.id}
-          onClick={() => onMovieClick(movie)}
-        >
+        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
       ))}
